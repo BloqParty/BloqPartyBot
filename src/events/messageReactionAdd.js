20c2dd3fe50c89a9cbedf7e0478af761b80f4c1c
NuggoDEV <NuggoDEV@gmail.com>
2023-11-02 06:45:35 +0000

    read json if id is correct, add colour roles

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -6,16 +6,33 @@ module.exports = {
     {
         if (user.bot) return;
 
-        const roleIDs = JSON.parse(fs.readFileSync("./src/extras/reactroles.json"));
-        if (reaction.emoji.name in roleIDs && reaction.message.id === "1154863931676971048")
+        if (reaction.message.id === "1154863931676971048")
         {
-            const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
-            const roles = guild.roles.cache;
-            const member = await guild.members.fetch(user.id);
+            const roleIDs = JSON.parse(fs.readFileSync("./src/extras/reactroles.json"));
+            if (reaction.emoji.name in roleIDs)
+            {
+                const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
+                const roles = guild.roles.cache;
+                const member = await guild.members.fetch(user.id);
 
-            member.roles.add(roles.get(roleIDs[reaction.emoji.name]));
-            console.log(`[Events | messageReactionAdd] Adding "${roles.get(roleIDs[reaction.emoji.name]).name}" to ${user.username} (${user.id})`);
+                member.roles.add(roles.get(roleIDs[reaction.emoji.name]));
+                console.log(`[Events | messageReactionAdd] Adding "${roles.get(roleIDs[reaction.emoji.name]).name}" to ${user.username} (${user.id})`);
+            }
         }
+
+        if (reaction.message.id === "1169521695439208533")
+        {
+            const roleIDs = JSON.parse(fs.readFileSync("./src/extras/colourroles.json"));
+            if (reaction.emoji.id in roleIDs)
+            {
+                const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
+                const roles = guild.roles.cache;
+                const member = await guild.members.fetch(user.id);
+                member.roles.add(roles.get(roleIDs[reaction.emoji.id]));
+                console.log(`[Events | messageReactionAdd] Adding "${roles.get(roleIDs[reaction.emoji.id]).name}" to ${user.username} (${user.id})`);
+            }
+        }
+        
         else if (reaction.emoji.name === "✅" && reaction.message.id === "1146607622980636803")
         {
             const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -6,16 +6,34 @@ module.exports = {
     {
         if (user.bot) return;
 
-        const roleIDs = JSON.parse(fs.readFileSync("./src/extras/reactroles.json"));
-        if (reaction.emoji.name in roleIDs && reaction.message.id === "1154863931676971048")
+        if (reaction.message.id === "1154863931676971048")
         {
-            const guild = await reaction.client.guilds.fetch("1096778383217664001");
-            const roles = guild.roles.cache;
-            const member = await guild.members.fetch(user.id);
+            const roleIDs = JSON.parse(fs.readFileSync("./src/extras/reactroles.json"));
+            if (reaction.emoji.name in roleIDs)
+            {
+                const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
+                const roles = guild.roles.cache;
+                const member = await guild.members.fetch(user.id);
 
-            member.roles.remove(roles.get(roleIDs[reaction.emoji.name]));
-            console.log(`[Events | messageReactionRemove] Removing "${roles.get(roleIDs[reaction.emoji.name]).name}" from ${user.username} (${user.id})`);
+                member.roles.remove(roles.get(roleIDs[reaction.emoji.name]));
+                console.log(`[Events | messageReactionRemove] Removing "${roles.get(roleIDs[reaction.emoji.name]).name}" from ${user.username} (${user.id})`);
+            }
         }
+
+        else if (reaction.message.id === "1169521695439208533")
+        {
+            const roleIDs = JSON.parse(fs.readFileSync("./src/extras/colourroles.json"));
+            if (reaction.emoji.id in roleIDs)
+            {
+                const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
+                const roles = guild.roles.cache;
+                const member = await guild.members.fetch(user.id);
+
+                member.roles.remove(roles.get(roleIDs[reaction.emoji.id]));
+                console.log(`[Events | messageReactionRemove] Removing "${roles.get(roleIDs[reaction.emoji.id]).name}" from ${user.username} (${user.id})`);
+            }
+        }
+
         else if (reaction.emoji.name === "✅" && reaction.message.id === "1146607622980636803")
         {
             const guild = await reaction.client.guilds.fetch(process.env.GUILD_ID);
