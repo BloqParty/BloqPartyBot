523277b81e4d8e96efad6c6772938449639f3af9
Riley Prince <NuggoDEV@gmail.com>
2024-03-05 14:58:46 +1030

    Fixed mistake causing bot to crash on filter remove (screw you curly braces)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -14,9 +14,9 @@ module.exports = {
             if (message.member.roles.resolveId(process.env.MOD_ROLE) || message.member.roles.resolveId(process.env.ADMIN_ROLE))
                 return message.channel.send(addPhraseToFilter(message.content.slice(12)) ? "Added `" + message.content.slice(12) + "` to the filter" : "Phrase is already on the filter");
 
-        //if (message.content.startsWith("!filter remove"))
-        //    if (message.member.roles.resolveId(process.env.MOD_ROLE) || message.member.roles.resolveId(process.env.ADMIN_ROLE))
-        //        return message.channel.send(removePhraseFromFilter(message.content.slice(15) ? "Removed `" + message.content.slice(15) + "` from the filter" : "Phrase wasn't on the filter"));
+        if (message.content.startsWith("!filter remove"))
+            if (message.member.roles.resolveId(process.env.MOD_ROLE) || message.member.roles.resolveId(process.env.ADMIN_ROLE))
+                return message.channel.send(removePhraseFromFilter(message.content.slice(15)) ? "Removed `" + message.content.slice(15) + "` from the filter" : "Phrase wasn't on the filter");
 
         if (await isMessageInFilter(message) === false)
             return;
