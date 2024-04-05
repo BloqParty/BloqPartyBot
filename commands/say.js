736c7ae0e6f4010f1b3f48819f2e8e4b78231a25
Riley Prince <NuggoDEV@gmail.com>
2024-04-06 01:59:29 +1030

    say command

---
+++ commands/say.js
@@ -0,0 +1,26 @@
+const { registerCommand } = require("../events/messageCreate");
+const { GetPrisma } = require("../src/prisma");
+const { Message } = require("discord.js");
+const client = require("../src/client");
+
+/**
+ * 
+ * @param { Message<boolean> } message 
+ * @returns { { "roleId": string, "emojiId": string, "messageId": string} | null }
+ */
+async function sayMessage(message)
+{
+    await message.delete();
+    await message.channel.sendTyping();
+
+    const msg = message.content.slice(5);
+    if (msg)
+        message.channel.send(msg);
+}
+
+module.exports = {
+    init() 
+    {
+        registerCommand("!say", sayMessage);
+    }
+}
